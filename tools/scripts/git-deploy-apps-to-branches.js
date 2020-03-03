f659455ccd8a3eb6931391c65471167e27108352
Erko Bridee <erko.bridee@gmail.com>
2020-03-03 18:15:31 +0100

    feat(tools): initial script code to help deploy the apps to git branches

---
+++ tools/scripts/git-deploy-apps-to-branches.js
@@ -0,0 +1,13 @@
+// const exec = require('./libs/execute-sync-command');
+const [appsDir = '', environment = 'dev'] = require('./libs/get-cli-args');
+
+if (!appsDir) {
+  process.exit();
+}
+
+console.log({
+  appsDir,
+  environment
+});
+
+// TODO: define the execution code flow
