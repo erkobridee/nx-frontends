843d97f86bd1ed10deabec17b6f4558673625e6f
Erko Bridee <erko.bridee@gmail.com>
2020-03-03 16:00:44 +0100

    update tools script : serve-dist-app.js

--- tools/scripts/serve-dist-app.js
+++ tools/scripts/serve-dist-app.js
@@ -1,10 +1,30 @@
-const exec = require('./libs/execute-sync-command');
-const [appName = ''] = require('./libs/get-cli-args');
+const [appName = '', port = '3000'] = require('./libs/get-cli-args');
 
 if (!appName) {
   process.exit();
 }
 
-exec(`npx http-server dist/apps/${appName} -o`, {
-  stdio: [0, 1, 2]
+const handler = require('serve-handler');
+const http = require('http');
+const open = require('open');
+
+const server = http.createServer((request, response) => {
+  // You pass two more arguments for config and middleware
+  // More details here: https://github.com/zeit/serve-handler#options
+  return handler(request, response, {
+    public: `dist/apps/${appName}`,
+    rewrites: [
+      {
+        source: '**',
+        destination: '/index.html'
+      }
+    ]
+  });
+});
+
+const httpPort = Number(port);
+server.listen(httpPort, () => {
+  const appUrl = `http://localhost:${httpPort}`;
+  console.log(`Running at ${appUrl}`);
+  open(appUrl);
 });
