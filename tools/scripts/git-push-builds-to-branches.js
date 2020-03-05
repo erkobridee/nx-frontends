139595a9c440d23f54ba77250f8ae5caba6b8df7
Erko Bridee <erko.bridee@gmail.com>
2020-03-05 16:45:17 +0100

    feat(tools): script lib to list the directories names from a given path

--- tools/scripts/git-push-builds-to-branches.js
+++ tools/scripts/git-push-builds-to-branches.js
@@ -1,4 +1,6 @@
-const exec = require('./libs/execute-sync-command');
+// const exec = require('./libs/execute-sync-command');
+const listDirectoriesFrom = require('./libs/list-directories-from');
+
 const [appsDir = '', environment = 'dev'] = require('./libs/get-cli-args');
 
 if (!appsDir) {
@@ -8,11 +10,15 @@ if (!appsDir) {
 // Why Using reduce() to Sequentially Resolve Promises Works
 // https://css-tricks.com/why-using-reduce-to-sequentially-resolve-promises-works/
 
+console.log('');
+
 console.log({
   appsDir,
   environment
 });
 
-exec(`ls ${appsDir}`, {
-  stdio: [0, 1, 2]
-});
+console.log('');
+
+console.log(listDirectoriesFrom(appsDir));
+
+console.log('');
