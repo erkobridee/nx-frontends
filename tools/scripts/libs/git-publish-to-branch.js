43121b3b231c0093eadc939cb9e61eeef6eed88d
Erko Bridee <erko.bridee@gmail.com>
2020-03-06 10:09:52 +0100

    update(tools): git push to branch lib

--- tools/scripts/libs/git-publish-to-branch.js
+++ tools/scripts/libs/git-publish-to-branch.js
@@ -2,12 +2,24 @@ const ghPages = require('gh-pages');
 
 function publish(path, branch) {
   return new Promise((resolve, reject) => {
-    ghPages.publish(path, { branch, history: false }, err => {
-      if (err) {
-        return reject(err);
+    ghPages.publish(
+      path,
+      {
+        branch,
+        history: false,
+        message: 'Auto-generated commit',
+        user: {
+          name: 'Azure Pipelines',
+          email: 'azuredevops@microsoft.com'
+        }
+      },
+      err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
       }
-      resolve();
-    });
+    );
   });
 }
 
