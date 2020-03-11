49ace3a196668e82f21b976ea9e0cfc8b36134cb
Erko Bridee <erko.bridee@gmail.com>
2020-03-11 10:57:02 +0100

    feat(jest): add junit and coverage reports

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,25 @@ module.exports = {
     '^.+\\.svg$': `${jestHelpersDir}/svgrMock.js`,
     '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `${jestHelpersDir}/fileMock.js`
   },
-  coverageReporters: ['html']
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '!**/jest.config.js',
+    '!**/src/polyfills.ts',
+    '!**/src/main.tsx',
+    '!**/src/environments/**',
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**'
+  ],
+  coverageReporters: ['cobertura', 'html'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '<rootDir>/reports/junit',
+        outputName: 'test-report.xml'
+      }
+    ]
+  ]
 };
