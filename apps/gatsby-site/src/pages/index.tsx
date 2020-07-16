e4e2f6e95da5a6aad0fb80f2c9be5e73f8e8255e
Erko Bridee <erko.bridee@gmail.com>
2020-07-16 09:33:09 +0200

    feat(gatsby site): define and use the gatsby image component

--- apps/gatsby-site/src/pages/index.tsx
+++ apps/gatsby-site/src/pages/index.tsx
@@ -5,6 +5,12 @@ import Layout from '@nx-frontends/shared/ui-components/Layout';
 import { ReactComponent as Logo } from '@nx-frontends/shared/assets/images/logo.svg';
 import { ReactComponent as Star } from '@nx-frontends/shared/assets/images/star.svg';
 
+import {
+  GatsbyAstronautImage,
+  GatsbyLogoImage,
+  NxLogoImage,
+} from '../components/Image';
+
 import '@nx-frontends/shared/assets/styles-site.scss';
 import './_styles.scss';
 
@@ -14,12 +20,17 @@ export const Index = () => (
       <p>content of the gatsby-site...</p>
     </main>
     <br />
-    <span>SVG images</span>
     <div className="svg-images">
+      <span>SVG images: </span>
       <Logo width="75" height="75" />
       <Star width="45" height="45" />
     </div>
     <br />
+    <div className="gatsby-images">
+      <GatsbyAstronautImage />
+      <GatsbyLogoImage />
+      <NxLogoImage />
+    </div>
   </Layout>
 );
 
