ec5b302e130058c7c04f9f72ff3917371798215c
Erko Bridee <erko.bridee@gmail.com>
2020-11-09 17:19:32 +0100

    update(storybook): global webpack config to load sass and also to map the libs/shared/assets

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 /**
  * Export a function. Accept the base config as the only param.
  * @param {Object} options
@@ -6,6 +9,42 @@
  */
 module.exports = async ({ config, mode }) => {
   // Make whatever fine-grained changes you need
+
+  const tsPaths = new TsconfigPathsPlugin({
+    configFile: './tsconfig.base.json',
+  });
+
+  config.resolve.plugins
+    ? config.resolve.plugins.push(tsPaths)
+    : (config.resolve.plugins = [tsPaths]);
+
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1,
+          modules: {
+            mode: 'local',
+          },
+          localsConvention: 'camelCase',
+        },
+      },
+      'sass-loader',
+    ],
+    include: path.resolve(__dirname, '../'),
+  });
+
+  // Alternately, for an alias:
+  config.resolve.alias = {
+    '@nx-frontends/shared/assets': path.resolve(
+      __dirname,
+      '../',
+      'libs/shared/assets'
+    ),
+  };
 
   // Return the altered config
   return config;
