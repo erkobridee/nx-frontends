a521510cfba96c4d264f337d8e7c18e181af479d
Erko Bridee <erko.bridee@gmail.com>
2020-11-09 16:51:56 +0100

    update(tools scripts): nx code related

--- tools/scripts/nx-calculate-commands.js
+++ tools/scripts/nx-calculate-commands.js
@@ -5,40 +5,40 @@ const LAST_COMMITS_AMOUNT = 1;
 const headParam = branchName === 'merge' ? 'HEAD' : `origin/${branchName}`;
 let baseParam = '';
 switch (branchName) {
-  case 'master':
-  case 'staging':
-  case 'develop':
-    baseParam = `origin/${branchName}~${LAST_COMMITS_AMOUNT}`;
-    break;
-  default:
-    baseParam = 'origin/develop';
-    break;
+	case 'master':
+	case 'staging':
+	case 'develop':
+		baseParam = `origin/${branchName}~${LAST_COMMITS_AMOUNT}`;
+		break;
+	default:
+		baseParam = 'origin/develop';
+		break;
 }
 
 const getAffectedProjects = (target = 'build') => {
-  return {
-    [target]: JSON.parse(
-      exec(
-        `npx nx print-affected --base=${baseParam} --head=${headParam} --target=${target}`
-      )
-    ).tasks.map((t) => t.target.project),
-  };
+	return {
+		[target]: JSON.parse(
+			exec(
+				`npx nx print-affected --base=${baseParam} --head=${headParam} --target=${target}`
+			)
+		).tasks.map((t) => t.target.project),
+	};
 };
 
 let output = '';
 if (
-  ['lint', 'test', 'build-storybook', 'build'].includes(target.toLowerCase())
+	['lint', 'test', 'build-storybook', 'build'].includes(target.toLowerCase())
 ) {
-  output = JSON.stringify({
-    ...getAffectedProjects(target),
-  });
+	output = JSON.stringify({
+		...getAffectedProjects(target),
+	});
 } else {
-  output = JSON.stringify({
-    ...getAffectedProjects('lint'),
-    ...getAffectedProjects('test'),
-    ...getAffectedProjects('build-storybook'),
-    ...getAffectedProjects('build'),
-  });
+	output = JSON.stringify({
+		...getAffectedProjects('lint'),
+		...getAffectedProjects('test'),
+		...getAffectedProjects('build-storybook'),
+		...getAffectedProjects('build'),
+	});
 }
 
 console.log(output);
--- tools/scripts/nx-run-many.js
+++ tools/scripts/nx-run-many.js
@@ -1,50 +1,50 @@
 const exec = require('./libs/execute-sync-command');
 const [
-  commandsStr = '[]',
-  target = '',
-  environment = 'development',
+	commandsStr = '[]',
+	target = '',
+	environment = 'development',
 ] = require('./libs/get-cli-args');
 const commands = JSON.parse(commandsStr);
 const projects = commands[target];
 
 if (!projects) {
-  process.exit();
+	process.exit();
 }
 
 if (['build', 'build-storybook'].includes(target)) {
-  require('./libs/fs-toolkit').removeSync('dist');
+	require('./libs/fs-toolkit').removeSync('dist');
 }
 
 if (target === 'test') {
-  require('./libs/fs-toolkit').removeSync('coverage');
+	require('./libs/fs-toolkit').removeSync('coverage');
 }
 
 const buildOutput = ['prod', 'production'].includes(environment)
-  ? ' --prod'
-  : '';
+	? ' --prod'
+	: '';
 
 const parallel = target !== 'build-storybook' ? ' --parallel' : '';
 
 try {
-  exec(
-    `npx nx run-many --target=${target}${buildOutput} --projects=${projects.join(
-      ','
-    )}${parallel}`,
-    {
-      stdio: [0, 1, 2],
-    }
-  );
+	exec(
+		`npx nx run-many --target=${target}${buildOutput} --projects=${projects.join(
+			','
+		)}${parallel}`,
+		{
+			stdio: [0, 1, 2],
+		}
+	);
 
-  if (target === 'test') {
-    require('./libs/merge-cobertura')();
-  }
+	if (target === 'test') {
+		require('./libs/merge-cobertura')();
+	}
 
-  console.log('\n\n');
-  console.log('success');
-  console.log('\n\n');
+	console.log('\n\n');
+	console.log('success');
+	console.log('\n\n');
 } catch (e) {
-  console.log('\n\n');
-  console.error('failure');
-  console.log('\n\n');
-  process.exit(1);
+	console.log('\n\n');
+	console.error('failure');
+	console.log('\n\n');
+	process.exit(1);
 }
