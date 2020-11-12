b24cfefd91dfe5236f831b654350281d030aacfc
Erko Bridee <erko.bridee@gmail.com>
2020-11-12 11:15:18 +0100

    update(tools scripts): libs execute sync command

--- tools/scripts/libs/execute-sync-command.js
+++ tools/scripts/libs/execute-sync-command.js
@@ -1,7 +1,13 @@
 const execSync = require('child_process').execSync;
 
 const exec = (cmd, options = undefined) => {
-	const output = cmd ? execSync(cmd, options) : undefined;
+	let output;
+	try {
+		output = cmd ? execSync(cmd, options) : undefined;
+	} catch (e) {
+		console.error(e);
+	}
+
 	if (!output) {
 		return '';
 	}
